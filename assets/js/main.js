bfe162fd1324b90e3834fee3d62143ec274e3565
evanzummeren <erikvanzummeren@gmail.com>
2015-06-03 20:26:52 +0200

    Update 2015-06-03T18:26:50.051Z

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,4 @@
-// ScrollMagic init
-var control = new ScrollMagic.Controller({
-	    globalSceneOptions: {
-	        triggerHook: 'onLeave'
-	    }
-	});
 
-// iPhone back to stylesheet position
-var scene = new ScrollMagic.Scene({
-triggerElement: ".videoBlock"
-
-})
-.setTween(console.log("bla"))
-.addIndicators()
-.loglevel(1)
-.addTo(control);
 
 
 TweenMax.to(".jumbotron", .5, {
@@ -44,12 +29,6 @@ TweenMax.to(".hdat-zeefakkel", 1.5, {
 });
 
 // Content
-var lichtBlok = TweenMax.to(".lightBlock", 1.5, {
-	height: 400,
-	delay: 1.7, 
-	opacity: 1,
-	ease: Expo.easeOut
-});
 TweenMax.to(".darkBlock", 1.5, {
 	height: 400,
 	delay: 1.7, 
@@ -62,6 +41,29 @@ TweenMax.to(".videoBlock", 1.5, {
 	opacity: 1,
 	ease: Expo.easeOut
 });
+
+// Timelines
+var shipTl = new TimelineMax();
+
+shipTl.to(".lightBlock", 1.5, {height: 400, opacity: 1, ease: Expo.easeOut})
+	.from(".hdat-ship", 1, {width: '47%', marginLeft: -50, opacity: 0, ease: Expo.easeOut})
+	.to(".vocBlock", 1, {height: 400, opacity: 1, ease: Expo.easeOut}, 0);
+
+// ScrollMagic init
+var control = new ScrollMagic.Controller({
+	    globalSceneOptions: {
+	        triggerHook: 'onLeave'
+	    }
+	});
+
+// Scrollmagic scenes
+var scene = new ScrollMagic.Scene({
+	triggerElement: ".videoBlock",
+	duration: '30%'
+})
+.setTween(shipTl)
+.addIndicators().loglevel(3)
+.addTo(control);
 
 
 /* ========================================================================
