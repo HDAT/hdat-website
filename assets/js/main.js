051530c17de041cbb3db0fc61c5d992bb10b3f5f
evanzummeren <erikvanzummeren@gmail.com>
2015-06-07 17:24:19 +0200

    Update 2015-06-07T15:24:16.853Z

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,35 +48,35 @@ var methodology = TweenMax.from(".methodology", 1, {
 });
 
 // Timelines
-var shipTl = new TimelineMax();
+// var shipTl = new TimelineMax();
 
-shipTl.to(".lightBlock", 1.5, {height: 400, opacity: 1, ease: Expo.easeOut})
-	.from(".hdat-ship", 1, {width: '47%', marginLeft: -50, opacity: 0, ease: Expo.easeOut})
-	.to(".vocBlock", 1, {height: 400, opacity: 1, ease: Expo.easeOut}, 0);
+// shipTl.to(".lightBlock", 1.5, {height: 400, opacity: 1, ease: Expo.easeOut})
+// 	.from(".hdat-ship", 1, {width: '47%', marginLeft: -50, opacity: 0, ease: Expo.easeOut})
+// 	.to(".vocBlock", 1, {height: 400, opacity: 1, ease: Expo.easeOut}, 0);
 
 // ScrollMagic init
-var control = new ScrollMagic.Controller({
-	    globalSceneOptions: {
-	        triggerHook: 'onLeave'
-	    }
-	});
+// var control = new ScrollMagic.Controller({
+// 	    globalSceneOptions: {
+// 	        triggerHook: 'onLeave'
+// 	    }
+// 	});
 
 // Scrollmagic scenes
-var vocScene = new ScrollMagic.Scene({
-	triggerElement: ".firstBlock",
-	duration: '30%'
-})
-.setTween(shipTl)
-.addIndicators().loglevel(3)
-.addTo(control);
+// var vocScene = new ScrollMagic.Scene({
+// 	triggerElement: ".firstBlock",
+// 	duration: '30%'
+// })
+// .setTween(shipTl)
+// .addIndicators().loglevel(3)
+// .addTo(control);
 
-var methodologyScene = new ScrollMagic.Scene({
-	triggerElement: ".vocBlock",
-	duration: '30%'
-})
-.setTween(methodology)
-.addIndicators().loglevel(3)
-.addTo(control);
+// var methodologyScene = new ScrollMagic.Scene({
+// 	triggerElement: ".vocBlock",
+// 	duration: '30%'
+// })
+// .setTween(methodology)
+// .addIndicators().loglevel(3)
+// .addTo(control);
 
 
 /* ========================================================================
